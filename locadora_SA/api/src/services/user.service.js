9bff3e6f97515c2029d100096f89e3cabb6a4fec
francagiovanna <francamariagiovanna4@gmail.com>
2026-06-15 17:06:24 -0300

    feat: mexi em algumas coisas no back-end e separei o repositorio da Locadora do geral do semestre

--- locadora_SA/api/src/services/user.service.js
+++
@@ -1,8 +0,0 @@
-export const validateUsuario = ({ nome, email }) => {
-  if (!nome || !email) {
-    throw new Error("Nome e e-mail são obrigatórios")
-  }
-  if (!email.includes("@")) {
-    throw new Error("E-mail inválido")
-  }
-} 
